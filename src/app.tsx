ca86a1081b17b9df8c83a081ce6aab47cbe83727
tonypinkevych <tony@aninix.com>
2025-06-18 17:26:48 +0200

    refactor: rename steps

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { default as BaseApp } from './steps/base'
-import { default as ContextApp } from './steps/context'
-import { default as RtkApp } from './steps/rtk'
-import { default as ZustandApp } from './steps/zustand'
+import { default as BaseApp } from './steps/1-base'
+import { default as ContextApp } from './steps/2-context'
+import { default as ZustandApp } from './steps/3-zustand'
+import { default as RtkApp } from './steps/4-rtk'
 
 function App(): React.JSX.Element {
   return (
